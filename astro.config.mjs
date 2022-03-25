16824735a08fe57b0e06185ad1c2f98d6346192a
Cyril Bralet <cyril.bralet@sanofi.com>
2022-03-25 13:08:13 +0100

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,16 @@ export default {
   // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
   buildOptions: {
     // site: 'http://example.com',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-    sitemap: true,         // Generate sitemap (set to "false" to disable)
+    site: 'https://cbralet.github.io/astro-test',
+    // Generate sitemap (set to "false" to disable)
+    sitemap: true
   },
   devOptions: {
     // hostname: 'localhost',  // The hostname to run the dev server on.
     // port: 3000,             // The port to run the dev server on.
   },
   renderers: [
-    "@astrojs/renderer-react",
-    "@astrojs/renderer-vue"
-  ],
-};
+    '@astrojs/renderer-react',
+    '@astrojs/renderer-vue'
+  ]
+}
